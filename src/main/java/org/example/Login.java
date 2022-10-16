c7459bcd2a07f1619c8e13816386f9b5226a49ff
mohoshi.haque@therapbd.net <mohoshihaque@gmail.com>
2022-10-17 00:11:49 +0600

    initial commit

---
+++ src/main/java/org/example/Login.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.openqa.selenium.By.xpath;
+
+public class Login {
+
+    @Test
+    public void login() {
+
+        System.setProperty("webdriver.chrome.driver", "F:\\Automation\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        String url = "https://www.daraz.com.bd/";
+        driver.get(url);
+        WebElement e1 = driver.findElement(xpath("//*[@id=\"anonLogin\"]/a"));
+        e1.click();
+
+        //TC-01
+        WebElement e2 = driver.findElement(xpath("//*[@id=\"container\"]/div/div[2]/form/div/div[2]/div[1]/button"));
+        e2.click();
+        String Expectresultemail = "You can't leave this empty.";
+        String Actualresultemail = driver.findElement(xpath("//*[@id=\"container\"]/div/div[2]/form/div/div[1]/div[1]/span")).getText();
+        Assert.assertEquals(Actualresultemail, Expectresultemail);
+
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Login l1 =new Login();
+        l1.login();
+    }
+}
